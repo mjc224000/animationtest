72bea3badab831bc6614882b10fd61d4ef334607
Administrator <397938635@qq.com>
2018-06-19 17:31:23 +0800

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,53 @@
+import React, {Component} from 'react';
+import Tween from 'rc-tween-one';
+import propTypes from 'prop-types';
+import imgUrl from './img/sjb.jpg'
+import './App.css';
+
+class Block extends Component {
+    static PropTypes = {
+        x: propTypes.number.isRequired,
+        y: propTypes.number.isRequired,
+        width: propTypes.number.isRequired,
+        height: propTypes.number.isRequired,
+        img:propTypes.string.isRequired
+    }
+
+    constructor(props) {
+        super(props)
+    }
+
+    render() {
+        const {x, y, width, height} = this.props;
+
+        return (
+            <ul className={'block'}>
+                <li style={{background:`url(${imgUrl})   ` }} className="front"></li>
+                <li className="back"></li>
+                <li className="top"></li>
+                <li className="bottom"></li>
+                <li className="left"></li>
+                <li className="right"></li>
+            </ul>)
+    }
+}
+
+class App extends Component {
+    makeBlock() {
+
+    }
+
+    render() {
+        return (
+            <div className={'wrap'}>
+                <Tween animation={[{translateX: 200, duration: 2000},
+                    {translateY: 100}
+                ]}>
+
+                    <div> 里面放依托是你觉得怎么样</div>
+                </Tween>
+            </div>)
+    }
+}
+
+export default App;
