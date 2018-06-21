fccfce77dd8b415bed007443abc4fb1e896ec49c
Administrator <397938635@qq.com>
2018-06-21 11:48:05 +0800

    ini

--- src/App.js
+++ src/App.js
@@ -3,16 +3,21 @@ import Tween from 'rc-tween-one';
 import propTypes from 'prop-types';
 import './App.css';
 import imgUrl from './img/sjb.jpg'
-import aUrl from './img/another.jpg';
+import aUrl from './img/another.jpg'
+const Separate = 'Separate'
+var store = {
+    count: 0,
+    arr: [],
+    animateData: []
+}
+
 class Block extends Component {
     static propTypes = {
         x: propTypes.number.isRequired,
         y: propTypes.number.isRequired,
         width: propTypes.number.isRequired,
         height: propTypes.number.isRequired,
-        img: propTypes.string.isRequired,
-        deX: propTypes.number.isRequired,
-        deY: propTypes.number.isRequired
+        frontImg: propTypes.string.isRequired,
     }
 
     constructor(props) {
@@ -20,8 +25,7 @@ class Block extends Component {
     }
 
     render() {
-        const {x, y, width, height, img, delay, deX, deY} = this.props;
-
+        const {x, y, width, height, frontImg} = this.props;
         return (
             <Tween style={{
                 position: 'absolute', left: x, top: y,
@@ -29,29 +33,12 @@ class Block extends Component {
                 perspective: '2000px',
                 transformOrigin: '50% 50%',
                 width: width,
-                height: height,
-                delay: 100
+                height: height
             }}
-
-                   animation={[
-                       {
-                           x: deX,
-                           y: deY,
-                           rotateX: Math.random() * 360 + 'deg',
-                           rotateY: Math.random() * 360 + 'deg',
-                           ease: 'easeOutElastic',
-                           duration: 2000
-                       },
-                       {rotateY: 0, rotateX: 0, duration: 200, delay},
-                       {x: 0, y: 0, ease: 'easeOutElastic', duration: 2000},
-                       {rotateX: 180, rotate: 180, delay: delay + 2000, onComplete: this.props.onComplete}
-                       /*    {rotateX: '180deg',rotateY:'180deg', duration: 2000, ease: "easeOutBounce", delay},*/
-
-                   ]}
-            >
+                   animation={this.props.animation} >
                 <ul className={'block'}>
                     <li style={{
-                        backgroundImage: `url(${img}) `,
+                        backgroundImage: `url(${frontImg}) `,
                         width: width, height: height,
                         backgroundPosition: -x + 'px ' + -y + 'px',
                         backgroundSize: '1200px 600px'
@@ -79,24 +66,66 @@ class Block extends Component {
     }
 }
 
-var store={
-    count:0,
-    arr:[],
-}
 
 class App extends Component {
     static defaultProps = {
         column: 4,
         row: 6,
         width: 1200,
-        height: 600
+        height: 600,
+    }
+
+    componentWillMount() {
+        const {column, row} = this.props;
+        for (var i = 0; i < column; i++) {
+            for (var j = 0; j < row; j++) {
+                this.getSeparate({x: i, y: j})
+            }
+        }
+    }
+
+    constructor(props) {
+        super(props);
+        this.state = {frontImg: imgUrl, total: 0}
+        this.getSeparate = this.getSeparate.bind(this);
+    }
+
+    getSeparate({x, y}) {
+        var key = x + '_' + y;
+        if (!store.animateData[Separate]) {
+            store.animateData[Separate] = [];
+        }
+        if (store.animateData[Separate][key]) {
+            return store.animateData[Separate][key]
+        }
+        const {width, height, column, row} = this.props
+
+        var centerX = width / 2,
+            centerY = height / 2,
+            blockWidth = width / column,
+            blockHeight = height / row,
+            dX = x * blockWidth,
+            dY = y * blockHeight;
+        var data = [{
+            x: (dX - centerX) * Math.random() * 2,
+            y: (dY - centerY) * Math.random() * 2,
+            rotateX: Math.random() * 360 + 'deg',
+            rotateY: Math.random() * 360 + 'deg',
+            ease: 'easeOutElastic',
+            duration: 2000
+        },
+            {rotateX: 0, rotateY: 0, ease: 'easeOutElastic', duration: 200},
+            {x: 0, y: 0, ease: 'easeOutElastic', duration: 2000,onComplete:this.onComplete}
+        ]
+        store.animateData[Separate][key] = data
+        return data
     }
 
     makeBlock() {
         const {column, row, width, height} = this.props;
         var blockWidth = width / column,
-            blockHeight = height / row,
-            res = [];
+            blockHeight = height / row;
+        var res = [];
         for (var i = 0; i < column; i++) {
             for (var j = 0; j < row; j++) {
                 res.push({
@@ -104,47 +133,42 @@ class App extends Component {
                     height: blockHeight,
                     x: i * blockWidth,
                     y: j * blockHeight,
-                    img: imgUrl,
-                    delay: parseInt(Math.random() * 2000)
+                    key: i + '_' + j
                 })
             }
         }
         return res
     }
 
-    onComplete(v) {
-
-        v.target['isFirst']=false;
-        const {column, row} = this.props,
-            total = column * row;
-        store.count++;
-        if (store.count == total) {
-            console.log(store.count);
+    onComplete = (() => {
+        const {column, row} = this.props;
+       const total = column * row;
+       let count=0;
+        return ()=>{
+            count++
+            if(count===total)
+            setTimeout(() => this.setState({frontImg: aUrl}))
         }
-    }
+    })()
 
-    getDistance({x, y}) {
-        var centerX = this.props.width / 2,
-            centerY = this.props.height / 2;
-        return {
-            deX: (x - centerX) * Math.random() * 2,
-            deY: (y - centerY) * Math.random() * 2
-        }
-    }
+    /*    onComplete() {
+            const {column, row} = this.props,
+                total = column * row;
+            store.count++;
+            if (store.count === total) {
+                setTimeout(() => this.setState({frontImg: aUrl}, () => store.count = 0))
+            }
+        }*/
 
     render() {
         var blockArr = this.makeBlock();
-        console.log(blockArr);
 
         return (
             <div className={'wrap'}>
                 {blockArr.map(v => {
-                    const {x, y} = v,
-                        {deX, deY} = this.getDistance({x, y});
-                    return <Block {...v} onComplete={this.onComplete.bind(this)} deX={deX} deY={deY}/>
+                    return <Block key={v.key} {...v} frontImg={this.state.frontImg} onComplete={this.onComplete}
+                                  animation={store.animateData[Separate][v.key]}/>
                 })}
-
-
             </div>)
     }
 }
